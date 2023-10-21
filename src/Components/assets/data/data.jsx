0caab3110c7134c753f2296f82e898aa8f013fc0
Sebastian4090 <bastek4090@gmail.com>
2023-10-21 19:25:01 +0200

    Upload Main Files + Main Page

---
+++ src/Components/assets/data/data.jsx
@@ -0,0 +1,37 @@
+import p1_img from './photos/p1_img.jpg';
+import p2_img from './photos/p2_img.jpg';
+import p3_img from './photos/p3_img.jpg';
+import p4_img from './photos/p4_img.jpg';
+
+let data_product = [
+    {
+        id: 1,
+        name: "Placeholder 1",
+        image: p1_img,
+        new_price: 46.00,
+        old_price: 92.00
+    },
+    {
+        id: 2,
+        name: "Placeholder 2",
+        image: p2_img,
+        new_price: 50.00,
+        old_price: 104.00
+    },
+    {
+        id: 3,
+        name: "Placeholder 3",
+        image: p3_img,
+        new_price: 30.00,
+        old_price: 42.00
+    },
+    {
+        id: 4,
+        name: "Placeholder 4",
+        image: p4_img,
+        new_price: 66.50,
+        old_price: 82.00
+    }
+]
+
+export default data_product;
---
+++ src/Components/assets/newCollection/new_collection.jsx
@@ -0,0 +1,69 @@
+import p1_img from './photos/nc1_img.jpg';
+import p2_img from './photos/nc2_img.jpg';
+import p3_img from './photos/nc3_img.jpg';
+import p4_img from './photos/nc4_img.jpg';
+import p5_img from './photos/nc5_img.jpg';
+import p6_img from './photos/nc6_img.jpg';
+import p7_img from './photos/nc7_img.jpg';
+import p8_img from './photos/nc8_img.jpg';
+
+let data_product = [
+    {
+        id: 1,
+        name: "Placeholder 1",
+        image: p1_img,
+        new_price: 50.00,
+        old_price: 55.00
+    },
+    {
+        id: 2,
+        name: "Placeholder 2",
+        image: p2_img,
+        new_price: 30.00,
+        old_price: 40.00
+    },
+    {
+        id: 3,
+        name: "Placeholder 3",
+        image: p3_img,
+        new_price: 45.00,
+        old_price: 48.00
+    },
+    {
+        id: 4,
+        name: "Placeholder 4",
+        image: p4_img,
+        new_price: 70.00,
+        old_price: 70.00
+    },
+    {
+        id: 5,
+        name: "Placeholder 5",
+        image: p5_img,
+        new_price: 60.00,
+        old_price: 65.00
+    },
+    {
+        id: 6,
+        name: "Placeholder 6",
+        image: p6_img,
+        new_price: 60.00,
+        old_price: 66.00
+    },
+    {
+        id: 7,
+        name: "Placeholder 7",
+        image: p7_img,
+        new_price: 80.00,
+        old_price: 80.00
+    },
+    {
+        id: 8,
+        name: "Placeholder 8",
+        image: p8_img,
+        new_price: 125.00,
+        old_price: 125.00
+    }
+]
+
+export default data_product;
