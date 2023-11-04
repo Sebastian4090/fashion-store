7bbf866365cf61fa1c6d2bdcb3c34af2394adee9
Sebastian4090 <bastek4090@gmail.com>
2023-11-04 15:12:08 +0100

    register page function

--- src/Container/App.jsx
+++ src/Container/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from '../Components/navbar/Navbar.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Store from '../Pages/Store';
 import ShopCategory from '../Pages/StoreCategory';
 import Product from '../Pages/Product';
@@ -12,7 +13,36 @@ import menBanner from '../Components/assets/banners/bannerMen_img.png';
 import womenBanner from '../Components/assets/banners/bannerWomen_img.png';
 import kidsBanner from '../Components/assets/banners/bannerKids_img.png';
 
+
 function App() {
+
+  const [route, setRoute] = useState('');
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const loadUser = (data) => {
+    setId(data.id);
+    setName(data.name);
+    setEmail(data.email);
+  }
+
+  const onRouteChange = (route) => {
+    if (route === 'signOut') {
+      setRoute('');
+      setId('');
+      setName('');
+      setEmail('');
+      setPassword('');
+      setIsSignedIn(false);
+
+    } else if (route === 'signedIn') {
+      setIsSignedIn(true);
+    }
+    setRoute(route);
+  }
 
   return (
     <div>
@@ -20,14 +50,14 @@ function App() {
         <Navbar />
         <Routes>
             <Route path='/' element={<Store/>}/>
-            <Route path='/men' element={<ShopCategory banner={menBanner} text="For Men" category="men"/>}/>
+            <Route path='/men' element={<ShopCategory banner={menBanner} text="For Men" category="men" test={name}/>}/>
             <Route path='/women' element={<ShopCategory banner={womenBanner} text="For Women"category="women"/>}/>
             <Route path='/kids' element={<ShopCategory banner={kidsBanner} text="For Kids" category="kids"/>}/>
             <Route path='/product' element={<Product/>}>
               <Route path=':productId' element={<Product/>}/>
             </Route>
             <Route path='/cart' element={<Cart/>}/>
-            <Route path='/sign-up' element={<SignUp/>}/>
+            <Route path='/sign-up' element={<SignUp onRouteChange={onRouteChange} loadUser={loadUser}/>}/>
             <Route path='/login' element={<Login/>}/>
         </Routes>
         <Footer/>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,21 +1,65 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './CSS/SignUp.css';
 
-const SignUp = () => {
+const SignUp = (props) => {
+
+    const [name, setName] = useState([]);
+    const [email, setEmail] = useState([]);
+    const [password, setPassword] = useState([]);
+
+    const onNameChange = (event) => {
+        setName(event.target.value);
+        console.log('name', name);
+    }
+
+    const onEmailChange = (event) => {
+        setEmail(event.target.value);
+        console.log('email', email)
+    }
+
+    const onPasswordChange = (event) => {
+        setPassword(event.target.value);
+        console.log('password', password);
+    }
+
+    const onSubmitSignIn = () => {
+        if (document.getElementById('agreement').checked) { 
+            fetch('localhost:5174/sign-up', {
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    password: password
+                })
+            })
+            .then(response => response.json())
+            .then(user => {
+                if (user.id) {
+                    props.loadUser(user);
+                    props.onRouteChange('signedIn');
+                }
+            })
+        } else {
+            alert('You must agree to the terms of use & privacy policy!');
+        }
+    }
+
     return ( 
         <div className="loginReg">
             <div className="loginReg-container">
-                <h1>Register</h1>
+                <h1>Register {name}</h1>
                 <div className="loginReg-fields">
-                    <input type="text" placeholder="Your Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
+                    <input type="text" placeholder="Your Name" name="name" id="name" onChange={onNameChange}/>
+                    <input type="email" placeholder="Email Address" name="email-address" id="email-address" onChange={onEmailChange}/>
+                    <input type="password" placeholder="Password" name="password" id="password" onChange={onPasswordChange}/>
                 </div>
-                <button>Continue</button>
+                <button onClick={onSubmitSignIn}>Continue</button>
                 <p className="loginReg-login">Already have an account?  
                 <Link style={{ textDecoration: 'none'}}  to='/login'><span>Login here!</span></Link></p>
                 <div className="loginReg-agree">
-                    <input type="checkbox" name="" id="" />
+                    <input type="checkbox" name="agreement" id="agreement" />
                     <p>I agree to the terms of use & privacy policy.</p>
                 </div>
             </div>
